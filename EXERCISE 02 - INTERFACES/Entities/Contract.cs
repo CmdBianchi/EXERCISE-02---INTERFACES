25fb573f23dc29fafb7dffb7c0cd6035baa4507e
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-23 15:13:18 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 02 - INTERFACES/Entities/Contract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Entities {
+    //------------------------------- START -------------------------------//
+    class Contract {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double Totalvalue { get; set; }
+        public List<Installment> Installments { get; set; }
+    //------------------------------- CONST -------------------------------//
+        public Contract(int number, DateTime date, double totalvalue, List<Installment> installments) {
+            Number = number;
+            Date = date;
+            Totalvalue = totalvalue;
+            Installments = new List<Installment>();
+        }
+    //------------------------------- MET -------------------------------//
+        public void AddInstallment(Installment installment) {
+            Installments.Add(installment);
+
+        }
+    }
+    //-------------------------------- END -------------------------------//
+}
